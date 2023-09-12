082bf49d3d68148dd12527e3a889814ff58c93ef
Audrey Main <aam6039@rit.edu>
2023-09-12 15:14:29 -0400

    added constructors

--- Pokemon/PokemonClone/Pokemon.cs
+++ Pokemon/PokemonClone/Pokemon.cs
@@ -8,14 +8,13 @@ namespace PokemonClone
 {
     internal class Pokemon
     {
-        static int EV = 42;
+        const int EV = 42;
 
         #region Member Variables
         private Species m_species;
         private string m_nickname;
 
         private int m_level;
-        //private int exp;
         private int m_currentHP;
 
         private int m_hp;
@@ -36,6 +35,26 @@ namespace PokemonClone
         #endregion
 
         #region Constructors
+        public Pokemon(Species a_species, int a_level)
+        {
+            m_species = a_species;
+            m_nickname = "";
+            m_level = a_level;
+        }
+
+        public Pokemon(Species a_species, int a_level, Move[] a_moveset)
+        {
+            m_species = a_species;
+            m_nickname = "";
+            m_level = a_level;
+
+            if (a_moveset.Length > 4)
+            {
+                for (int i = 0; i < 4; i++) { m_moveset[i] = a_moveset[i]; }
+            }
+            else m_moveset = a_moveset;
+        }
+
         public Pokemon(Species a_species, string a_name, int a_level)
         {
             m_species = a_species;
@@ -43,10 +62,11 @@ namespace PokemonClone
             m_level = a_level;
         }
 
-        public Pokemon(Species a_species, string a_name, Move[] a_moveset)
+        public Pokemon(Species a_species, string a_name, int a_level, Move[] a_moveset)
         {
             m_species = a_species;
             m_nickname = a_name;
+            m_level = a_level;
 
             if (a_moveset.Length > 4)
             {
@@ -57,7 +77,13 @@ namespace PokemonClone
         #endregion
 
         #region Properties
-        public string Name { get { return m_nickname; } }
+        public string Name { 
+            get {
+                if (m_nickname != "") 
+                    return m_nickname;
+                else
+                    return m_species.Name; }
+        }
         public Type PrimaryType { get { return m_species.PrimaryType; } }
         public Type SecondaryType { get { return m_species.SecondaryType; } }
         #endregion
