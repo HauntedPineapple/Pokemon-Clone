cf92f673ee92434ecee754374d1dcfe8a46dc077
Audrey Main <aam6039@rit.edu>
2023-09-12 16:26:01 -0400

    Added natures

--- Pokemon/PokemonClone/Program.cs
+++ Pokemon/PokemonClone/Program.cs
@@ -12,43 +12,81 @@
     {
         static void Main(string[] args)
         {
-            CreateTypes();
-            CreateMoves();
-            CreateNatures();
-            CreateSpecies();
-            Console.WriteLine("Hello, World!");
+            //CreateTypes();
+            //CreateMoves();
+            //CreateSpecies();
+
+            Dictionary<string, Dictionary<int, string>> natures = new Dictionary<string, Dictionary<int, string>>();
+            CreateNatures(natures);          
+
+            foreach(KeyValuePair<string, Dictionary<int, string>> kvp in natures)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (KeyValuePair<int, string> entry in kvp.Value)
+                    Console.WriteLine("\n\tKey = {0}, Value = {1}",
+                                  entry.Key, entry.Value);
+            }
+
+            Console.WriteLine("");
         }
 
-
-        static void CreateNatures()
+        static void CreateNatures(Dictionary<string, Dictionary<int, string>> a_natures)
         {
-            Dictionary<string, int> natureValue = new Dictionary<string, int>();
-            natures.Add("NATURE", natureValue);
+            // a 1 int value indicates the corresponding stat is increased by 10%
+            // a -1 int value indicates the corresponding stat is decreased by 10%
+            a_natures.Add("Hardy", new Dictionary<int, string> { { 1, "Attack" }, { -1, "Attack" } });
+            a_natures.Add("Lonely", new Dictionary<int, string> { { 1, "Attack" }, { -1, "Defense" } });
+            a_natures.Add("Adamant", new Dictionary<int, string> { { 1, "Attack" }, { -1, "Special Attack" } });
+            a_natures.Add("Naughty", new Dictionary<int, string> { { 1, "Attack" }, { -1, "Special Defense" } });
+            a_natures.Add("Brave", new Dictionary<int, string> { { 1, "Attack" }, { -1, "Speed" } });
+            
+            a_natures.Add("Bold", new Dictionary<int, string> { { 1, "Defense" }, { -1, "Attack" } });
+            a_natures.Add("Docile", new Dictionary<int, string> { { 1, "Defense" }, { -1, "Defense" } });
+            a_natures.Add("Impish", new Dictionary<int, string> { { 1, "Defense" }, { -1, "Special Attack" } });
+            a_natures.Add("Lax", new Dictionary<int, string> { { 1, "Defense" }, { -1, "Special Defense" } });
+            a_natures.Add("Relaxed", new Dictionary<int, string> { { 1, "Defense" }, { -1, "Speed" } });
+            
+            a_natures.Add("Modest", new Dictionary<int, string> { { 1, "Special Attack" }, { -1, "Attack" } });
+            a_natures.Add("Mild", new Dictionary<int, string> { { 1, "Special Attack" }, { -1, "Defense" } });
+            a_natures.Add("Bashful", new Dictionary<int, string> { { 1, "Special Attack" }, { -1, "Special Attack" } });
+            a_natures.Add("Rash", new Dictionary<int, string> { { 1, "Special Attack" }, { -1, "Special Defense" } });
+            a_natures.Add("Quiet", new Dictionary<int, string> { { 1, "Special Attack" }, { -1, "Speed" } });
+            
+            a_natures.Add("Calm", new Dictionary<int, string> { { 1, "Special Defense" }, { -1, "Attack" } });
+            a_natures.Add("Gentle", new Dictionary<int, string> { { 1, "Special Defense" }, { -1, "Defense" } });
+            a_natures.Add("Careful", new Dictionary<int, string> { { 1, "Special Defense" }, { -1, "Special Attack" } });
+            a_natures.Add("Quirky", new Dictionary<int, string> { { 1, "Special Defense" }, { -1, "Special Defense" } });
+            a_natures.Add("Sassy", new Dictionary<int, string> { { 1, "Special Defense" }, { -1, "Speed" } });
+            
+            a_natures.Add("Timid", new Dictionary<int, string> { { 1, "Speed" }, { -1, "Attack" } });
+            a_natures.Add("Hasty", new Dictionary<int, string> { { 1, "Speed" }, { -1, "Defense" } });
+            a_natures.Add("Jolly", new Dictionary<int, string> { { 1, "Speed" }, { -1, "Special Attack" } });
+            a_natures.Add("Naive", new Dictionary<int, string> { { 1, "Speed" }, { -1, "Special Defense" } });
+            a_natures.Add("Serious", new Dictionary<int, string> { { 1, "Speed" }, { -1, "Speed" } });
         }
 
-        static Dictionary<string, Dictionary<string, int>> natures;
         #region Types
         static Type? type_normal;
-        #endregion
         static void CreateTypes()
         {
 
         }
+        #endregion
 
         #region Moves
         static Move? move_flamethrower;
-        #endregion
         static void CreateMoves()
         {
 
         }
+        #endregion
 
         #region Species
         static Species? species_bulbasaur;
-        #endregion
         static void CreateSpecies()
         {
 
         }
+        #endregion
     }
 }
